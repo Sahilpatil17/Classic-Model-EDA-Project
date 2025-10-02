c25216b9154f57b32e88724f4ef8e274b52103f5
Sahil Patil <sahilpatil17147919@gmail.com>
2025-10-02 22:56:51 +0530

    Update Part 4 Subqueries & insights.sql
    Added remaining questions

--- Part 4 Subqueries & insights.sql
+++ Part 4 Subqueries & insights.sql
@@ -31,3 +31,47 @@ WHERE
 
 #-------------------------------------------------------------------------
 # 3. Find the employee with the highest number of direct reports.
+#Code---
+SELECT 
+    e.employeeNumber,
+    e.firstName,
+    e.lastName,
+    e.reportsTo,
+    COUNT(r.employeeNumber) AS Direct_report_to
+FROM
+    employees e
+        LEFT JOIN
+    employees r ON e.employeeNumber = r.reportsTo
+GROUP BY e.employeeNumber , e.firstName , e.lastName
+ORDER BY Direct_report_to DESC
+LIMIT 5;
+
+#-------------------------------------------------------------------------
+# 4. Show orders that contain the most expensive product.
+#Code---
+SELECT DISTINCT
+    p.productCode, p.productName, p.MSRP
+FROM
+    orderdetails o
+        JOIN
+    products p ON p.productCode = o.productCode
+ORDER BY MSRP DESC;
+
+#-------------------------------------------------------------------------
+# 5. List the top 3 offices with the highest total sales.
+#Code---
+SELECT 
+    o.officeCode,
+    o.city,
+    o.country,
+    SUM(p.amount) AS Total_sales
+FROM
+    offices o
+        JOIN
+    employees e ON o.officeCode = e.officeCode
+        JOIN
+    customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+        JOIN
+    payments p ON c.customerNumber = p.customerNumber
+GROUP BY o.officeCode , o.city , o.country
+ORDER BY Total_sales DESC;
